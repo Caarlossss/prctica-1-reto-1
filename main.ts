3138af94f6a72fdd958279bdcf3f0b21cf54dfda
Caarlossss <119401398+Caarlossss@users.noreply.github.com>
2022-11-29 09:18:06 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+basic.showString("Esto es para vosotros jugadores")
+basic.forever(function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # . # .
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # . . .
+        . . # # .
+        . . . . #
+        . . # # .
+        # # . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . # # . .
+        # . . . .
+        . # # . .
+        . . . # #
+        `)
+})
